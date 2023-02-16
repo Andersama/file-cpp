656f1dbef3b7535b98b7ae123cea0448796272e3
Andersama <anderson.john.alexander@gmail.com>
2023-02-16 00:03:04 -0800

    Add basic UTF8 utility functions TODO: wide and other formats

---
+++ file-cpp/file-cpp.cpp
@@ -0,0 +1,17 @@
+﻿// file-cpp.cpp : Defines the entry point for the application.
+//
+
+#include "file.h"
+#include <filesystem>
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+	const wchar_t* prefix = L"x:";
+	cout << std::filesystem::_Has_drive_letter_prefix(prefix, prefix + 2);
+	cout << util::wide::has_drive_letter_prefix(prefix, prefix + 2);
+	cout << "Hello CMake." << endl;
+	return 0;
+}
